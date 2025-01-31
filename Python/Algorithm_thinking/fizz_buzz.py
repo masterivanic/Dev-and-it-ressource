19e0f1d131c8c8daa0f42f14b5f9f1704c3484b7
Ivanic <62161915+masterivanic@users.noreply.github.com>
2025-01-31 13:35:20 +0100

    chore(Python): add some kata algorithm resolve

--- Python/Algorithm_thinking/fizz_buzz.py
+++ Python/Algorithm_thinking/fizz_buzz.py
@@ -11,6 +11,9 @@
     number that we know which are divided by 5 and 3
 
 """
+import re
+
+from collections import Counter
 
 for value in range(1, 101):
     if value % 5 == 0 and value % 3 == 0:
@@ -19,6 +22,22 @@ for value in range(1, 101):
         print('buzz')
     elif value % 3 == 0:
         print('fizz')
+
+
+def to_camel_case(text:str):
+    """Convert string to camel case https://www.codewars.com/kata/517abf86da9663f1d2000003/python"""
+    words = re.split(r'[-_]', text)
+    return words[0] + ''.join(word.capitalize() for word in words[1:])
+
+
+def find_deleted_number(arr, mixed_arr):
+    """ lost number algo: (https://www.codewars.com/kata/595aa94353e43a8746000120/train/python)"""
+    return sum(arr) - sum(mixed_arr)
+
+
+def duplicate_count(text:str):
+    """Counting Duplicates: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python"""
+    return sum(1 for count in Counter(text.lower()).values() if count >= 2)
     
 
     
