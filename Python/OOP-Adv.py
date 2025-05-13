2482a37ff5feb97abd503c54ddbc6c1bcc9ba870
Ivanic <62161915+masterivanic@users.noreply.github.com>
2025-05-13 15:20:03 +0200

    chore: Add python interface practice

--- Python/OOP-Adv.py
+++ Python/OOP-Adv.py
@@ -129,6 +129,106 @@ class PluginRegistry:
             return self.available_plugins.get(name)
         return self.activate_plugins.get(name)
 
+
+class InformalParserInterface:
+
+  def load_data_source(self, path:str, file_name:str) -> str:
+    pass
+
+  def extract_text(self, file_name:str) -> dict:
+    pass
+
+
+class PdfParser(InformalParserInterface):
+  def load_data_source(self, path:str, file_name:str) -> str:
+    """Overrides InformalParserInterface.load_data_source()"""
+    pass
+
+  def extract_text(self, file_name:str) -> dict:
+    """Overrides InformalParserInterface.extract_text()"""
+    pass
+
+
+class EmailParser(InformalParserInterface):
+  def load_data_source(self, path:str, file_name:str) -> str:
+    pass
+
+  def extract_text(self, file_name:str) -> dict:
+    pass
+
+
+class ParserMeta(type):
+  
+  def __instancecheck_(cls, instance):
+    return cls.__subclasscheck__(type(instance))
+
+  def __subclasscheck__(cls, subclass):
+    return (hasattr(subclass, 'load_data_source') and 
+                callable(subclass.load_data_source) and 
+                hasattr(subclass, 'extract_text') and 
+                callable(subclass.extract_text))
+
+
+class UpdatedInformalParserInterface(metaclass=ParserMeta):
+    """This interface is used for concrete classes to inherit from.
+    There is no need to define the ParserMeta methods as any class
+    as they are implicitly made available via .__subclasscheck__().
+    """
+    pass
+
+
+class PdfParserNew:
+    """Extract text from a PDF."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides UpdatedInformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text(self, full_file_path: str) -> dict:
+        """Overrides UpdatedInformalParserInterface.extract_text()"""
+        pass
+
+class EmlParserNew:
+    """Extract text from an email."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides UpdatedInformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text_from_email(self, full_file_path: str) -> dict:
+        """A method defined only in EmlParser.
+        Does not override UpdatedInformalParserInterface.extract_text()
+        """
+        pass
+
+
+class PersonMeta(type):
+    """A person metaclass"""
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+
+    def __subclasscheck__(cls, subclass):
+        return (hasattr(subclass, 'name') and 
+                callable(subclass.name) and 
+                hasattr(subclass, 'age') and 
+                callable(subclass.age))
+
+class PersonSuper:
+    """A person superclass"""
+    def name(self) -> str:
+        pass
+
+    def age(self) -> int:
+        pass
+
+class Person(metaclass=PersonMeta):
+    """Person interface built from PersonMeta metaclass."""
+    pass
+
+class Employee(PersonSuper):
+    """Inherits from PersonSuper
+    PersonSuper will appear in Employee.__mro__
+    """
+    pass
+
 if __name__ == '__main__':
     obj = MyClass()
     # print(obj)
